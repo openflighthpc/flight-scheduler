ba424b4c87b9bf142db6ac53f5a00072e1647c5e
William McCumsite <william.mccumstie@gmail.com>
2020-09-16 13:21:09 +0100

    Split batch options into a magic variety
    The job scripts will be able to specify options as a magic comment,
    however they will not have ability to set the cart-blanche
    
    Instead the magical options are stored separately

--- lib/flight_scheduler/cli.rb
+++ lib/flight_scheduler/cli.rb
@@ -69,9 +69,13 @@ module FlightScheduler
       c.summary = 'List the available partitions'
     end
 
+    MAGIC_BATCH_SLOP = Slop::Options.new.tap do |slop|
+      slop.string '-N', '--nodes', 'The minimum number of required nodes'
+    end
+
     create_command 'batch', 'SCRIPT [ARGS...]' do |c|
       c.summary = 'Schedule a new job to be ran'
-      c.slop.string '-N', '--nodes', 'The minimum number of required nodes'
+      MAGIC_BATCH_SLOP.each { |opt| c.slop.send(:add_option, opt.dup) }
     end
 
     create_command 'cancel', 'JOBID' do |c|
